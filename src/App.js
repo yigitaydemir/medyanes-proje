4aa3caaa7eed51c81ca3cf033022fa58430aaad6
Yusuf Yiğit Aydemir <yusufyigitaydemir@gmail.com>
2024-03-31 19:23:59 +0300

    nav

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <div className=' w-full bg-slate-500 flex justify-center'>
+        <div className=' max-w-5xl w-full bg-red-300'>
+          <h1>BitGambit</h1>
+
+          <ul>
+            <li>Home</li>
+            <li>Home</li>
+            <li>Home</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
