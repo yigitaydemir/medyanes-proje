5a4165afefb9a700d04777b69ed9047e75faf8a1
Yusuf Yiğit Aydemir <yusufyigitaydemir@gmail.com>
2024-07-17 15:07:01 +0300

    devam

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,31 @@ import "./App.css";
 
 const App = () => {
   const [countries, setCountries] = useState([]);
-  const [capital, setCapital] = useState(null)
+  const [capital, setCapital] = useState(null);
+
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
-    const url = capital ? `https://restcountries.com/v3.1/capital/${capital}` : "https://restcountries.com/v3.1/all"
+    const url = capital
+      ? `https://restcountries.com/v3.1/capital/${capital}`
+      : "https://restcountries.com/v3.1/all";
 
     const getCountries = () => {
-      fetch(url)
-        .then((data) => data.json())
-        .then((json) => {
-          console.log(json);
-          setCountries(json);
-        });
+      setIsLoading(true);
+
+      try {
+        fetch(url)
+          .then((data) => data.json())
+          .then((json) => {
+            setCountries(json);
+          });
+      } catch (error) {
+        console.log(error.mesage);
+        setError(error.mesage);
+      } finally {
+        setIsLoading(false);
+      }
     };
 
     getCountries();
@@ -27,11 +40,12 @@ const App = () => {
     "Oslo",
     "Copenhagen",
     "Reykjavik",
+    "Display All",
   ];
 
   const handleChange = (e) => {
-    setCapital(e.target.value)
-  }
+    setCapital(e.target.value === "Display All" ? null : e.target.value);
+  };
 
   return (
     <div>
@@ -41,13 +55,24 @@ const App = () => {
         ))}
       </select>
 
-      {countries.map((country) => (
-        <CountryCard
-          key={country.name.common}
-          country={country.name.common}
-          capital={country.capital}
-        ></CountryCard>
-      ))}
+      {isLoading ? (
+        <div>
+          <h1>Loading...</h1>
+        </div>
+      ) : error ? (
+        <div>
+          <h1>There is an error, please try again later...</h1> <br />{" "}
+          <p>{error}</p>
+        </div>
+      ) : (
+        countries.map((country) => (
+          <CountryCard
+            key={country.name.common}
+            country={country.name.common}
+            capital={country.capital}
+          ></CountryCard>
+        ))
+      )}
     </div>
   );
 };
